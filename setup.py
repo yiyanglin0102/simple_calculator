8a07f568f939f71b7a04061d5ea5fe86c86e9b9b
yiyanglin0102 <yiyanglin0102@gmail.com>
2024-08-13 14:05:58 -0400

    Modified files

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='simple_calculator',
+    name='simple_calculator_yyl',
     version='0.1',
     packages=find_packages(),
     description='A simple calculator Python package',
